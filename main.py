abb09c67dcb26e9b5db75789a0cc2d42da46ac6b
Shazly11 <asshazly150@gmail.com>
2025-06-30 12:54:30 +0300

    added simple front page

--- main.py
+++ main.py
@@ -27,10 +27,15 @@
 # async def get_user(user_id: int):
 #     return {"user_id":user_id}
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from contextlib import asynccontextmanager
+
 from database import create_db_and_tables
 from routes.tasks import router as tasks_router
+
+templates = Jinja2Templates(directory="templates")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -40,7 +45,6 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(tasks_router)
 
-@app.get("/")
-def root():
-    return {"message": "Task API is running"}
-
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
